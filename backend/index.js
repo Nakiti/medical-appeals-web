4c3240c2f42dbf6d53aa6e71b33181ef88e4467f
Nakiti <nikhilakiti91@gmail.com>
2025-04-16 11:37:21 -0700

    meh

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const corsOptions = {
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Authorization'],
    credentials: true, 
-};
+}; 
 
 app.use(express.json())
 app.use(cors(corsOptions))
